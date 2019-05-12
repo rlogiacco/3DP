50afe33bff8820a76b217200c05e9669e23be27b
Roberto Lo Giacco <rlogiacco@gmail.com>
2019-05-12 11:58:11 +0200

    drops SD support

--- firmware/ender 3/mks gen l/Configuration_adv.h
+++ firmware/ender 3/mks gen l/Configuration_adv.h
@@ -678,7 +678,7 @@
  */
 #if ENABLED(DOGLCD)
   // Show SD percentage next to the progress bar
-  #define DOGM_SD_PERCENT
+  //#define DOGM_SD_PERCENT
 
   // Enable to save many cycles by drawing a hollow frame on the Info Screen
   #define XYZ_HOLLOW_FRAME
